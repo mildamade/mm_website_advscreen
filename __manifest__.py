f2e97bad93e6f6f8c6e609c2be505979f55e2a1e
Mildamade <milda.dvorak@gmail.com>
2019-01-04 21:28:37 +0100

    add css not func

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'website',
-    'version': '0.18',
+    'version': '0.19',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website'],
@@ -27,6 +27,9 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+    ],
+    'css': [
+        'static/css/adv.css'
     ],
     # only loaded in demonstration mode
     'demo': [
