fc8227a66c448a4cc2e3a428293113b9a09d2efe
Mildamade <milda.dvorak@gmail.com>
2019-01-07 19:00:39 +0100

    font a pozition

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'website',
-    'version': '0.26',
+    'version': '0.37',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website'],
