bea8b39043d0c04413e7dfd4020040fe24a3ebe8
Mildamade <milda.dvorak@gmail.com>
2019-01-11 16:10:09 +0100

    function in chrome

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'website',
-    'version': '0.53',
+    'version': '0.55',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'website'],
