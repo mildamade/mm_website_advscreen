aef8aab4cea77033b2940bd80441d6ee85c22f4e
Mildamade <milda.dvorak@gmail.com>
2019-01-13 03:14:21 +0100

    price and currency

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools import float_round
 
 # class mm_website_advscreen(models.Model):
 #     _name = 'mm_website_advscreen.mm_website_advscreen'
